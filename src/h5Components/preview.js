98134ab03a80b6eb5f0ff658768bbf4c14336de8
wibetter <365533093@qq.com>
2022-08-19 14:57:42 +0800

    feat: 新增单独H5自定义组件示例

---
+++ src/h5Components/preview.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import VueInfoCard from './info-card/info-card';
+import { createVue3Component } from 'vue3-aipage-widget';
+
+const InfoCard = createVue3Component(VueInfoCard);
+
+ReactDOM.render(<>
+  <InfoCard />
+</>, document.getElementById('root'));
